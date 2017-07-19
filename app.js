3290f2c399b2d047b5175de9417844ecec4e09a3
Reyn-L <Reyn.Leong@gmail.com>
2017-07-19 10:14:06 -1000

    post

--- app.js
+++ app.js
@@ -3,40 +3,41 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 let word = { buzzwords: [] };
+let pass = {success: true};
+let fail = {success:false};
+let urlencodedParser = bodyParser.urlencoded({extended:false});
+let jsonParser = bodyParser.json();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index.html');
-  console.log(res.body);
 });
-
 
 app.get('/buzzwords', (req, res) => {
   res.json(word);
-  console.log(res.url);
 });
 
-app.post('/buzzword/points', (req, res, next) => {
-  //res.send(req.body);
-  console.log(req.body.buzzword);
-  next();
-
+app.post('/buzzword', urlencodedParser, (req, res) => {
+  if(!req.body.buzzword) {
+    res.status(404).send('Not Found');
+  } else {
+    res.send(pass);
+    word.buzzwords.push(req.body.buzzword);
+  }
 });
 
-app.put('/buzzword', (req, res, next) => {
+// app.put('/buzzword', (req, res) => {
 
-});
+// });
 
-app.delete('/buzzword', (req, res) => {
+// app.delete('/buzzword', (req, res) => {
 
-});
+// });
 
-app.post('/reset', (req, res) => {
+// app.post('/reset', (req, res) => {
 
-});
+// });
 
 
 const server = app.listen(8080, () => {
