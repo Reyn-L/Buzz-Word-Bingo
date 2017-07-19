007eec24f4b0db3843e36aa21a7231b41cbb0adc
Reyn-L <Reyn.Leong@gmail.com>
2017-07-18 19:44:02 -1000

    get '/' amd body parser

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
 /*jshint esversion: 6*/
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+let wordbuzz= {};
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
 
 
+app.get('/buzzwords', (req, res) => {
+  console.log(res.body);
+});
 
-app.listen(8080);
+app.post('/buzzword', (req, res) => {
+  res.send(req.body);
+  console.log(res.body);
+
+});
+
+app.put('/buzzword', (req, res) => {
+
+});
+
+app.delete('/buzzword', (req, res) => {
+
+});
+
+app.post('/reset', (req, res) => {
+
+});
+
+
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('help', host, port);
+});
